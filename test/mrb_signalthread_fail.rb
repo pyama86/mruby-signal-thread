3de0e9de6d4f2be72d814ff59eec7350bad42c08
Uchio KONDO <udzura@pepabo.com>
2017-04-10 18:39:10 +0900

    Add test

---
+++ test/mrb_signalthread_fail.rb
@@ -0,0 +1,20 @@
+assert('SignalThread#failed') do
+  t = SignalThread.new { 1 + 1 }
+  usleep 10 * 1000
+  assert_false t.failed?
+
+  t = SignalThread.new { raise "Something" }
+  usleep 10 * 1000
+  assert_true t.failed?
+
+  t = SignalThread.new { nomethod }
+  usleep 10 * 1000
+  assert_true t.failed?
+end
+
+assert('SignalThread#exception') do
+  t = SignalThread.new { raise "Something" }
+  usleep 10 * 1000
+  assert_equal "RuntimeError", t.exception.class.to_s
+  assert_equal "Something", t.exception.message
+end
